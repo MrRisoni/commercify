5e35a81caf7f2062a146b2bb0907e38ee6c300ed
MrRisoni <vanguelisk@gmail.com>
2020-10-29 08:22:42 +0200

    hql for currencies

--- src/main/java/core/GeneralController.java
+++ src/main/java/core/GeneralController.java
@@ -1,6 +1,8 @@
 package core;
 
+import entity.HibernateUtil;
 import entity.general.Currencies;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import repositories.CurrencyRepo;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class GeneralController {
@@ -18,19 +21,20 @@ public class GeneralController {
     CurrencyRepo curRepo;
 
     @RequestMapping(value=  "/api/currencies" , method = RequestMethod.GET)
-    public ResponseEntity<Object> getCurrencies() {
+    public ResponseEntity<List<Currencies>> getCurrencies() {
         try {
-            return new ResponseEntity<>(curRepo.findAll(), HttpStatus.OK);
+            return new ResponseEntity<>( HibernateUtil.getEM().createQuery("SELECT new entity.general.Currencies(id,title,code) FROM Currencies",Currencies.class).getResultList(), HttpStatus.OK);
         }
         catch (Exception ex) {
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_GATEWAY);
+            ex.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.BAD_GATEWAY);
         }
     }
 
 
     @RequestMapping(value=  "/api/version" , method = RequestMethod.GET)
     public ResponseEntity<String> getVersion() {
-        
+
         return new ResponseEntity<>("4.5.0.11", HttpStatus.OK);
         
     }
